40c160709ed8d09008b0ca35b65c1b514389dd1c
tombiks <tncybaktir@gmail.com>
2025-11-21 08:42:44 +0300

    infrastructure/repo/unitofwork created

---
+++ eTicaret.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using eTicaret.Domain.Interfaces;
+using eTicaret.Infrastructure.Persistence.Context;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTicaret.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly eTicaretDbContext _context;
+
+        private IUserRepository _userRepositiory;
+        public UnitOfWork (eTicaretDbContext context)
+        {
+            _context = context;            
+        }
+
+        public IUserRepository Users 
+        {
+            get 
+            {
+                if (_userRepositiory == null) 
+                {
+                    _userRepositiory = new UserRepository(_context);
+                }
+
+                return _userRepositiory;
+            }
+        }
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            
+            System.GC.SuppressFinalize(this);
+        }
+    }
+}
