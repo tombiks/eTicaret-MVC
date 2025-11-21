55f5fa0b4074f47a0471581f2f271a877ba2ebda
tombiks <tncybaktir@gmail.com>
2025-11-21 06:39:00 +0300

    userrole enums

---
+++ eTicaret.Domain/Enums/UserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTicaret.Domain.Enums
+{
+    public enum UserRole
+    {
+        Admin = 1,
+        Customer = 2,
+        Seller = 3,
+
+    }
+}
