ee3ca6e7eeedeff104605b4c8246df59f6828ef2
tombiks <tncybaktir@gmail.com>
2025-11-21 08:11:58 +0300

    IUserRepository created

---
+++ eTicaret.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTicaret.Domain.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository Users { get; }
+
+        Task<int> CompleteAsync();
+    }
+}
