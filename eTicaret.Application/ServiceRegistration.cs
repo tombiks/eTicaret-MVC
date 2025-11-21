9066e43568c28a76dd611d5b17f88d9833f43265
tombiks <tncybaktir@gmail.com>
2025-11-21 18:11:26 +0300

    application/serviceregistration.cs created

---
+++ eTicaret.Application/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using eTicaret.Application.Interfaces;
+using eTicaret.Application.Services;
+
+namespace eTicaret.Application
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationServices(this IServiceCollection services) 
+        {
+            services.AddScoped<IUserService, UserService>();
+        }
+    
+    }
+}
