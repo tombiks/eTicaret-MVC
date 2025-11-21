f2379ba994e6af883b472e7a3c024e33746d5ac5
tombiks <tncybaktir@gmail.com>
2025-11-21 08:07:32 +0300

    IUserRepository created

---
+++ eTicaret.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using eTicaret.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTicaret.Domain.Interfaces
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<User> GetUserByEmailAsync(string email);
+        Task<IReadOnlyList<User>> GetUsersByRoleAsync(string roleName);
+    }
+}
